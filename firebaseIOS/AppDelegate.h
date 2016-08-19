90e49dbeeca31807c4ca746c3b679aab16af3f28
Badar Maqsood <badar@tintash.com>
2016-08-19 15:18:09 +0500

    initial commit

---
+++ firebaseIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firebaseIOS
+//
+//  Created by Tin on 8/19/16.
+//  Copyright © 2016 Tin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ firebaseIOS/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  firebaseIOS
+//
+
+//  Copyright (c) 2016 Tin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
